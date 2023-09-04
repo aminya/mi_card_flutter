41f54c710e3bd378e4ee444d0ddee39420a4d08a
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-09-04 01:22:04 -0700

    fix: build the web app using parcel

---
+++ .terserrc.mjs
@@ -0,0 +1,35 @@
+/** @type {import("terser").MinifyOptions} */
+const options = {
+  compress: {
+    global_defs: {
+      // remove dev and test specific code for production
+      "process.env.NODE_ENV": process.env.NODE_ENV || "production",
+      "process.env.BABEL_ENV": process.env.BABEL_ENV || process.env.NODE_ENV || "production",
+    },
+    toplevel: false, // results in errors
+    hoist_vars: false,
+    hoist_funs: true,
+    pure_getters: true,
+    unsafe: true,
+    unsafe_arrows: false, // results in errors
+    unsafe_comps: true,
+    unsafe_Function: true,
+    unsafe_math: true,
+    unsafe_symbols: true,
+    unsafe_methods: false, // results in errors
+    unsafe_proto: true,
+    unsafe_regexp: true,
+    unsafe_undefined: true,
+    passes: 3,
+  },
+  format: {
+    comments: false,
+  },
+  mangle: {
+    eval: true,
+    // _flutter is a top-level variable defined by flutter.js
+    toplevel: false,
+  },
+  sourceMap: false,
+}
+export default options
